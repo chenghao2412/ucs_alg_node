f4e9b445d2b52eb3307f386555200eccddb486d4
chenghao2412 <78187368+chenghao2412@users.noreply.github.com>
2024-05-23 15:01:06 +0800

    minio修改

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,8 +1,8 @@
 from src.ucs_alg_node.cli import *
 
-
 def test_mqtt_cli():
     mqtt_cli = MqttCli({
+
     })
 
     def on_connect():
@@ -17,8 +17,11 @@ def test_mqtt_cli():
     })
 
 def test_redis_cli():
-    redis_cli = RedisCli({})
-
+    redis_cli = RedisCli({
+        'host':'localhost',
+        'port':6379,
+        'password':'123456'
+    })
     redis_cli.connect()
 
     redis_cli.set('test', 'test')
@@ -45,21 +48,36 @@ def test_mq_cli():
 
 def test_minio_cli():
     """test for minio upload, download, query, count"""
-    minio_cli = MinioCli({
-        'host': 'localhost:9000',
-        'bucket':'ucs/alg/res',
-        'username': 'admin',
-        'passwd': 'admin123'
-    })
+    minio_cli = MinioCli(
+        'localhost',
+         '9090',
+        'pubilc',#这里好像只能起bucket名字，不能用文件夹
+         'admin',
+        'admin1234'
+    )
 
     fobj_list = minio_cli.query_all()
+    # print(fobj_list)
     for fobj in fobj_list:
         # todo: download all fobj_list
         # minio_cli.download()
         pass
 
+    # The file to upload, change this path if needed 本地文件上传
+    # source_file = r"D:\py program\ucs_alg_node\requirements.txt"
+    #
+    # destination_file = "requirements05.txt"
+    # minio_cli.upload(destination_file, source_file)
+    # The file to download, change this path if needed 文件下载
+    source_file = r"D:\py program\ucs_alg_node\requirements01.txt"
+    destination_file = "requirements05.txt"
+    minio_cli.download(destination_file,source_file)
+
+
+
 if __name__ == '__main__':
-    test_mqtt_cli()
-    test_redis_cli()
-    test_mqtt_cli()
+    # test_mqtt_cli()
+    # test_redis_cli()
+    # test_mqtt_cli()
+
     test_minio_cli()
